7761893fa5a191260563197944a7c5c3235f1e1a
pavankotha <pavankotha9550@outlook.com>
2026-07-18 14:03:10 +0530

    jenkins

---
+++ cicd-tools/data.tf
@@ -0,0 +1,17 @@
+
+data "aws_ami" "joindevops" {
+
+   most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["973714476881"]
+}
---
+++ cicd-tools/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "daws84s.cyou"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z09059862X9S9AE54Z9SP"
+  description = "description"
+}
